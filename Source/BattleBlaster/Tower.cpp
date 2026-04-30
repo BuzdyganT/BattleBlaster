de72df99dedbdd9a96c00a4ad3622f3ccaebcd42
Maciej <raniszem@gmail.com>
2026-04-30 21:17:49 +0200

    Enemy shooting every 2 seconds

--- Source/BattleBlaster/Tower.cpp
+++ Source/BattleBlaster/Tower.cpp
@@ -7,20 +7,36 @@ void ATower::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	
+	FTimerHandle FireTimerHandle;
+	GetWorldTimerManager().SetTimer(FireTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
 }
 
 void ATower::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
-	
+	if (IsInFireRange())
+	{
+		RotateTurret(Tank->GetActorLocation());
+	}
+}
+
+void ATower::CheckFireCondition()
+{
+	if (Tank && IsInFireRange() )
+	{
+		Fire();
+	}
+}
+
+bool ATower::IsInFireRange()
+{
+	bool Result = false;
 	if (Tank)
 	{
 		float DistatnceToTank = FVector::Dist(GetActorLocation(),Tank->GetActorLocation());
-		if (DistatnceToTank <= FireRange)
-		{
-			RotateTurret(Tank->GetActorLocation());
-		}
+		Result = (DistatnceToTank <= FireRange);
+
 	}
+	return Result;
 }
